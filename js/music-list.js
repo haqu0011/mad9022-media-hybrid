3e6d88e2d1f07830e4ef519bceadcc4d84f52cc1
Haqu0011@algonquinlive.com <82667805+FIRAKINA@users.noreply.github.com>
2022-03-11 23:51:28 -0500

    Hybrid 4

---
+++ js/music-list.js
@@ -0,0 +1,29 @@
+let allMusic = [
+	{
+		name: 'Bible Black',
+		artist: 'Scarlxrd',
+		img: 'media/images/deadrising.jpg',
+		src: 'media/audio/BB.mp4',
+	},
+
+	{
+		name: 'Stand Yxur Grxund',
+		artist: 'Scarlxrd',
+		img: 'media/images/dxxm.jpg',
+		src: 'media/audio/SYG.mp4',
+	},
+
+	{
+		name: 'Kill Cxnfirmed',
+		artist: 'Scarlxrd',
+		img: 'media/images/immortal.jpg',
+		src: 'media/audio/KC.mp4',
+	},
+
+	{
+		name: 'Living Legend',
+		artist: 'Scarlxrd',
+		img: 'media/images/infinity.jpg',
+		src: 'media/audio/LG.mp4',
+	},
+];
